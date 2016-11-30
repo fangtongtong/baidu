654adf05a7f82012351efc77170047b365d670a1
fangtongtong <1370791265@qq.com>
2016-11-30 08:43:10 +0800

    add

---
+++ js/baidu.js
@@ -0,0 +1,40 @@
+ $(function(){
+     var clientH=$(window).height();
+
+     var num=0;
+     var flag=true;
+     touch.on("body","swipeup",".fullpage",function(){
+         if(!flag){
+             return;
+         }
+         num++;
+         var les=$("section").length;
+         if(num==les){
+             num=les-1;
+             return;
+             flag=false;
+
+         }
+
+         $(".fullpage").css("marginTop",-num*clientH)
+     });
+
+     touch.on("body","swipedown",".fullpage",function(){
+         if(!flag){
+             return;
+         }
+         num--;
+         if(num==-1){
+             num=0;
+             return;
+             flag=false;
+
+         }
+
+         $(".fullpage").css("marginTop",-num*clientH)
+     });
+
+     $(".fullpage")[0].addEventListener("webkitTransition",function(){
+         flag=true;
+     })
+ });
